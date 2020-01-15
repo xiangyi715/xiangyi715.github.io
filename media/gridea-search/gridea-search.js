68d9c9025b8c634f50dd24024b71d9ec7974b477
xiangyi715 <xuxiangyi1993@gmail.com>
2020-01-15 09:43:57 +0800

    update from gridea: 2020-01-15 09:43:57

--- media/gridea-search/gridea-search.js
+++ media/gridea-search/gridea-search.js
@@ -34,18 +34,24 @@ function ajax(obj) {
 //模糊搜索
 function fuzzySearch(data, phrase) {
     var options = {
-        shouldSort: true,
-        includeMatches: true,
-        threshold: 0.5,
-        location: 0,
+        shouldSort: true,	// 是否按分数对结果列表排序
+        includeMatches: true,	//  是否应将分数包含在结果集中。0分表示完全匹配，1分表示完全不匹配。
+        threshold: 0.4,		// 匹配算法阈值。阈值为0.0需要完全匹配（字母和位置），阈值为1.0将匹配任何内容。
+/**
+         * 确定匹配与模糊位置（由位置指定）的距离。一个精确的字母匹配，即距离模糊位置很远的字符将被视为完全不匹配。
+         *  距离为0要求匹配位于指定的准确位置，距离为1000则要求完全匹配位于使用阈值0.8找到的位置的800个字符以内。
+         */
+        location: 0,		// 确定文本中预期找到的模式的大致位置。
         distance: 1000,
         maxPatternLength: 32,
         minMatchCharLength: 1,
+	// 搜索标题与作者名
         keys: [
             'title',
             'content'
         ]
     };
+	// 设置数据与参数
     var fuse = new Fuse(data, options);
     var fuzzyResult = fuse.search(phrase);
     return fuzzyResult;
