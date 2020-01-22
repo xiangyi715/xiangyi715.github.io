678c8db32f4dd3c11375572281ffc64be32fadff
xiangyi715 <xuxiangyi1993@gmail.com>
2020-01-22 16:53:38 +0800

    update from gridea: 2020-01-22 16:53:38

--- media/script/script.js
+++ media/script/script.js
@@ -245,17 +245,3 @@ document.ready(
         _Blog.toggleTheme();
     }
 );
-
-//放于底部
-    <script type="text/javascript">
-    //樱花
-    var system = {};
-    var p = navigator.platform;
-    system.win = p.indexOf("Win") == 0;
-    system.mac = p.indexOf("Mac") == 0;
-    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-        if (system.win || system.mac || system.xll) {//如果是电脑
-
-        $.getScript("https://amayaliu.cn/PersonalHabits/AmayaBlossoms.js");
-    } else { //如果是手机}
-    </script>
